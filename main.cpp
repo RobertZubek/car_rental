49b77c94d302b9ca775617844556c6e3c1dfacb4
Robert Zubek <robert.zubek.ofc@gmail.com>
2023-12-15 10:56:38 +0100

    Car class and out operator created

--- main.cpp
+++ main.cpp
@@ -104,6 +104,45 @@ public:
 
 };
 
+class Car
+{
+private:
+    string fstatus;
+    string fmodel;
+    string fdescription;
+    float fprice;
+public:
+    ~Car()=default;
+    Car(vector<Car>& cars, string status, string model, string description, float price)
+    {
+        fstatus=status;
+        fmodel=model;
+        fdescription=description;
+        fprice=price;
+    }
+
+    void set_status(string status){fstatus=status;}
+    void set_model(string model){fmodel=model;}
+    void set_description(string description){fdescription=description;}
+    void set_price(float price){fprice=price;}
+
+    string get_status(void)const{return fstatus;}
+    string get_model(void)const{return fmodel;}
+    string get_description(void)const{return fdescription;}
+    float get_price(void)const{return fprice;}
+
+
+};
+
+ostream& operator<< (ostream& out, const Car& car)
+{
+    out<<"Car model: "<<car.get_model()<<endl;
+    out<<"Car status: "<<car.get_model()<<endl;
+    out<<"Car description: "<<car.get_description()<<endl;
+    out<<"Car price per day: "<<car.get_price()<<endl;
+    return out;
+}
+
 
 void update_usr_number(vector<User>& users)
 {
