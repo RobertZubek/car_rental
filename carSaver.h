195bd1edbbdb9a5768acf09ddb3925421b5e26e0
Robert Zubek <robert.zubek.ofc@gmail.com>
2023-12-28 19:24:15 +0100

    loading method applied

--- carSaver.h
+++ carSaver.h
@@ -25,4 +25,29 @@ void save(vector<Car>& cars)
                 cout << "Failed to open txt file" << endl;
             }
 }
+
+vector<Car> read(vector<Car>& cars)
+{
+    ifstream file("cars.txt");
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            stringstream ss(line);
+            string status;
+            string model;
+            string description;
+            float price;
+            ss >> status >> model>>description>>price;
+            Car obj(status, model, description, price);
+            cars.push_back(obj);
+        }
+        file.close();
+        cout << "Vector of objects loaded from cars.txt" << endl;
+    }
+    else {
+        cout << "Failed to open cars.txt" << endl;
+    }
+    return cars;
+    
+}
 };
