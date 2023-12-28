a4aedb0b1502ead031b3e815d84cad44d1782e79
Robert Zubek <robert.zubek.ofc@gmail.com>
2023-12-28 19:30:36 +0100

    loading cars implemented

--- main.cpp
+++ main.cpp
@@ -99,17 +99,21 @@ int main(){
     userSaver userSave;
     carSaver carSave;
     rentSaver rentSave;
+
     vector<User> users;
     userSave.read(users);
     update_usr_number(users);
+
     vector<Admin> admins;
     adminSave.read(admins);
     update_admin_number(admins);
+
     vector<Car> cars;
+    carSave.read(cars);
+
     vector<Rent> rents;
-    Admin def_admin( "def",21,0,"0",false,"1111", true);
-    admins.push_back(def_admin);
-    update_admin_number(admins);
+    
+    
     bool c=true;
     while(c)
     {
